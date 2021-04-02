aecafae3b81a920cf192e41efc9ac3eb28f3d971
Marc Wren <things@soul-fresh.com>
2021-04-02 16:28:42 -0600

    Updated the build to automatically detect input paths.

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,20 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import commonjs from "rollup-plugin-commonjs";
+import glob from 'glob';
 
 import pkg from './package.json';
+
+const inputs = glob.sync('src/**/!(*.test|setupTests|index).?(js|jsx)');
+
+console.log('---- MATCHES ----');
+console.log(inputs);
 
 const external = Object.keys(pkg.peerDependencies || {})
   .concat(Object.keys(pkg.optionalDependencies || {}));
 
 module.exports = {
-  input: [
-    'src/index.js',
-    // List individual entry points here.
-    'src/components/buttons/UnstyledAction.jsx',
-    'src/components/buttons/RoutedAction.jsx',
-    'src/components/buttons/AnalyticsAction.jsx',
-  ],
+  input: ['src/index.js'].concat(inputs),
   output: [{
     dir: 'lib/esm',
     format: 'esm',
