0f246b660277f6421079d4d0a452172400952722
Marc Wren <things@soul-fresh.com>
2021-04-07 17:34:16 -0600

    Added Currency and Unit components to the numbers package index file.

--- src/components/numbers/index.js
+++ src/components/numbers/index.js
@@ -1,2 +1,6 @@
 export * from './number-util';
 export * from './NumberDisplay.jsx';
+export * from './Currency.jsx';
+export * from './CurrencyName.jsx';
+export * from './Unit.jsx';
+export * from './UnitName.jsx';
