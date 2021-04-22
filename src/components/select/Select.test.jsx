4f10452303dc0fab944daf84a46c35e71ae868b3
Marc Wren <things@soul-fresh.com>
2021-04-21 20:34:08 -0600

    Fixed failing test.

--- src/components/select/Select.test.jsx
+++ src/components/select/Select.test.jsx
@@ -2,17 +2,17 @@
 describe('Select', () => {
   describe('uncontrolled', () => {
     describe('with a function as children', () => {
-      xit('should render the trigger.');
-      xit('should be closed by default.');
+      xit('should render the trigger.', () => {});
+      xit('should be closed by default.', () => {});
 
       describe('after clicking the trigger', () => {
-        xit('should open the menu.');
+        xit('should open the menu.', () => {});
         xit('should call the onOpen callback.', () => {});
         xit('should not call the onClose callback.', () => {});
         xit('should call the trigger onClick handler.', () => {});
 
         describe('twice', () => {
-          xit('should close the menu.');
+          xit('should close the menu.', () => {});
           xit('should call the onClose callback.', () => {});
           xit('should not call the onOpen callback.', () => {});
           xit('should call the trigger onClick handler.', () => {});
@@ -21,13 +21,13 @@ describe('Select', () => {
     });
 
     describe('with an element as children', () => {
-      xit('should render the trigger.');
+      xit('should render the trigger.', () => {});
 
       describe('after clicking the trigger', () => {
-        xit('should open the menu.');
+        xit('should open the menu.', () => {});
 
         describe('twice', () => {
-          xit('should close the menu.');
+          xit('should close the menu.', () => {});
         });
       });
     });
