eea735be778c6950a6b3cc7a97ea3e59d18a2295
Marc Wren <things@soul-fresh.com>
2021-06-23 18:27:24 -0400

    Added the useLogPropChanges hook for easy debugging of component re-renders.

---
+++ src/hooks/debug/useLogPropChanges.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/**
+ * Console log changes to a component's props.
+ * @param {object} props
+ */
+export function useLogPropChanges(props) {
+  const prev = React.useRef(props);
+  React.useEffect(() => {
+    const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
+      if (prev.current[k] !== v) {
+        ps[k] = [prev.current[k], v];
+      }
+      return ps;
+    }, {});
+    if (Object.keys(changedProps).length > 0) {
+      console.log('Changed props:', changedProps);
+    }
+    prev.current = props;
+  });
+}
+
