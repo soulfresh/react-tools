c23fb69534734bba3cf54bf051c1f0b3073d2628
Marc Wren <things@soul-fresh.com>
2021-12-02 16:48:03 -0500

    Added a spec definition to the useEnterExit tests

--- src/hooks/useEnterExit.test.jsx
+++ src/hooks/useEnterExit.test.jsx
@@ -13,6 +13,10 @@ describe('useEnterExit', () => {
         xit('should set the exited state.', () => {});
         xit('should not be considered visible.', () => {});
       });
+
+      describe('and then a transition with the same name on a child element ends', () => {
+        xit('should not set the exited state.', () => {});
+      })
     });
   });
 
