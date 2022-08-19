c51303a1c2f05b3a639d959a6622759f0facdf66
Marc Wren <things@soul-fresh.com>
2022-08-19 14:32:47 -0400

    fix: useTimeout support for multiple timeouts
    Previously the `useTimeout` wrapper callback could only handle one
    `setTimeout` call at a time. Each additional callback would lose the
    previous timeout id. This fix now ensures that simultaneous timeouts
    are all cleaned up correctly.

--- src/hooks/useTimeout.js
+++ src/hooks/useTimeout.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 /**
- * Returns a wrapper around `setTimeout` that will
- * automatically clear the timeout if the component
- * is destroyed before the timeout. Use it in exactly
- * the same manner as `setTimeout`
+ * Returns a wrapper around `setTimeout` that will automatically clear the
+ * timeout if the component is destroyed before the timeout. Use the returned
+ * function in exactly the same manner as `setTimeout`.
+ *
+ * IMPORTANT: `useTimeout` returns a function that is a wrapper around
+ * `setTimeout`. This means you need to call the returned wrapper function where
+ * you need timeouts (as opposed to calling `useTimeout` where you need the
+ * timeouts). This has the advantage that you can use the returned function even
+ * in places where hooks can't be used (ex. conditional statements, inside
+ * callbacks or effects, etc).
  *
  * ```js
  * const wait = useTimeout();
@@ -16,22 +22,27 @@ import React from 'react';
  * @return {function}
  */
 export function useTimeout() {
-  const idRef = React.useRef();
+  const idRef = React.useRef([]);
 
   React.useEffect(() => {
     return () => {
       // When the component is destroyed,
       // if the timeout was created, then clear it.
-      if (idRef.current) {
-        clearTimeout(idRef.current);
-      }
+      idRef.current.forEach(id => {
+        clearTimeout(id);
+      })
     };
   }, []);
 
-  return (cb, delay, ...rest) => {
-    return idRef.current = setTimeout(() => {
-      idRef.current = null;
+  return React.useCallback((cb, delay, ...rest) => {
+    const t = setTimeout(() => {
+      idRef.current = idRef.current.filter(id => id !== t)
+
       cb(...rest);
     }, delay);
-  };
+
+    idRef.current.push(t);
+    return t;
+  }, []);
 }
+
--- src/hooks/useTimeout.test.jsx
+++ src/hooks/useTimeout.test.jsx
@@ -4,53 +4,106 @@ import { render } from '@testing-library/react';
 import { useTimeout } from './useTimeout';
 
 describe('useTimeout', () => {
-  let id, spy, unmount;
+  let id, id2, spy, unmount;
   const delay = 60;
 
-  const Component = ({ done }) => {
-    const timeout = useTimeout();
-    id = timeout(done, delay);
-    return null;
-  };
+  describe('with a single timeout', () => {
+    const Component = ({ done }) => {
+      const timeout = useTimeout();
+      id = timeout(done, delay);
+      return null;
+    };
 
-  beforeEach(() => {
-    spy = jest.fn();
+    beforeEach(() => {
+      spy = jest.fn();
 
-    ({unmount} = render(
-      <Component done={spy} />
-    ));
+      ({unmount} = render(
+        <Component done={spy} />
+      ));
+    });
+
+    describe('after the timeout', () => {
+      beforeEach((done) => {
+        // Wait for the delay to pass and then
+        // check to see if the spy was called.
+        setTimeout(done, delay);
+      });
+
+      it('should call the spy.', () => {
+        expect(spy).toHaveBeenCalledTimes(1);
+      });
+
+      it('should return a timeout id to cancel the timeout.', () => {
+        expect(id).toBeGreaterThan(0);
+      });
+    });
+
+    describe('before the timeout', () => {
+      it('should not have called the spy.', () => {
+        expect(spy).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('after being destroyed', () => {
+      beforeEach((done) => {
+        unmount();
+        setTimeout(done, delay * 2);
+      });
+
+      it('should not call the spy.', () => {
+        expect(spy).not.toHaveBeenCalled();
+      });
+    });
   });
 
-  describe('after the timeout', () => {
-    beforeEach((done) => {
-      // Wait for the delay to pass and then
-      // check to see if the spy was called.
-      setTimeout(done, delay);
+  describe('with multiple timeouts', () => {
+    const Component = ({ done }) => {
+      const timeout = useTimeout();
+      id = timeout(done, delay);
+      id2 = timeout(done, delay);
+      return null;
+    };
+
+    beforeEach(() => {
+      spy = jest.fn();
+
+      ({unmount} = render(
+        <Component done={spy} />
+      ));
     });
 
-    it('should call the spy.', () => {
-      expect(spy).toHaveBeenCalledTimes(1);
+    describe('after the timeout', () => {
+      beforeEach((done) => {
+        // Wait for the delay to pass and then
+        // check to see if the spy was called.
+        setTimeout(done, delay);
+      });
+
+      it('should call the spy twice.', () => {
+        expect(spy).toHaveBeenCalledTimes(2);
+      });
+
+      it('should return all timeout ids.', () => {
+        expect(id).toBeGreaterThan(0);
+        expect(id2).toBeGreaterThan(0);
+      });
     });
 
-    it('should return a timeout id to cancel the timeout.', () => {
-      expect(id).toBeGreaterThan(0);
-    });
-  });
-
-  describe('before the timeout', () => {
-    it('should not have called the spy.', () => {
-      expect(spy).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('after being destroyed', () => {
-    beforeEach((done) => {
-      unmount();
-      setTimeout(done, delay * 2);
+    describe('before the timeout', () => {
+      it('should not have called the spy.', () => {
+        expect(spy).not.toHaveBeenCalled();
+      });
     });
 
-    it('should not call the spy.', () => {
-      expect(spy).not.toHaveBeenCalled();
+    describe('after being destroyed', () => {
+      beforeEach((done) => {
+        unmount();
+        setTimeout(done, delay * 2);
+      });
+
+      it('should not call the spy.', () => {
+        expect(spy).not.toHaveBeenCalled();
+      });
     });
   });
 });
